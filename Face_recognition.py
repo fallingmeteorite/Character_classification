5f8a3c7f98eafd254a6b7b27729d242fc65d9008
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2024-10-24 23:58:08 +0800

    Update Face_recognition.py

--- Face_recognition.py
+++ Face_recognition.py
@@ -1,6 +1,12 @@
-from imgutils.metrics import ccip_difference, ccip_clustering, ccip_same
+import logging as log_use
+import colorlog
 
+from imgutils.metrics import ccip_difference, ccip_clustering, ccip_same
+from log import get_logger
+
+logging = get_logger(log_use.INFO)
 
 def detect_similarity(base_path, input_path):
     similarity_01 = ccip_difference(base_path, input_path)
+    # logging.info(similarity_01)
     return similarity_01 * 1000
