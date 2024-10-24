744abdd04cdd92d79fc13df24b5eccd4583166c1
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2024-10-24 23:58:10 +0800

    Update Initialize.py

--- Initialize.py
+++ Initialize.py
@@ -16,8 +16,9 @@ from log import get_logger
 
 logging = get_logger(log_use.INFO)
 
-
-def Initialize():
+# 传入是否开启该功能参数
+def Initialize(body_cut):
+    # 重新命名对比图片名称
     files_list = os.listdir('Recognize_faces')
     for i in tqdm(range(len(files_list))):
         try:
@@ -28,6 +29,7 @@ def Initialize():
         except Exception as error:
             logging.critical(error)
 
+    # 分类对比人脸到各个文件夹
     files_list = os.listdir('Recognize_faces')
     for i in tqdm(range(len(files_list))):
         try:
@@ -39,32 +41,40 @@ def Initialize():
         except Exception as error:
             logging.critical(error)
 
-    logging.info('Start dividing the characters...')
-    files_list = os.listdir('Categorize_pictures')
-    for i in tqdm(range(len(files_list))):
-        try:
+    # 开启则对多人图片进行处理，关闭则不会处理
+    if body_cut:
+        logging.info('Start dividing the characters...')
+        files_list = os.listdir('Categorize_pictures')
+        for i in tqdm(range(len(files_list))):
+            try:
+                file_input = files_list[i]
+                img = cv2.imread(f'Categorize_pictures\\{file_input}')
+                result = detect_person(f'Categorize_pictures\\{file_input}', iou_threshold=0.4)
+                for h in range(len(result)):
+                    coordinate_list = (result[h])[0]
+                    save_img = img[int(coordinate_list[1]):int(coordinate_list[3]),
+                               int(coordinate_list[0]):int(coordinate_list[2])]
+                    formatted_number = "{:02d}".format(h)
+                    file_input = file_input.split('.')[0]
+                    cv2.imwrite(f'Body_cache\\{file_input}_{formatted_number}.png', save_img)
+            except Exception as error:
+                logging.critical(error)
+        logging.info('The split is complete')
+    else:
+        files_list = os.listdir('Categorize_pictures')
+        for i in tqdm(range(len(files_list))):
             file_input = files_list[i]
-            img = cv2.imread(f'Categorize_pictures\\{file_input}')
-            result = detect_person(f'Categorize_pictures\\{file_input}', iou_threshold=0.4, max_infer_size=896)
-            for h in range(len(result)):
-                coordinate_list = (result[h])[0]
-                save_img = img[int(coordinate_list[1]):int(coordinate_list[3]),
-                           int(coordinate_list[0]):int(coordinate_list[2])]
-                formatted_number = "{:02d}".format(h)
-                file_input = file_input.split('.')[0]
-                cv2.imwrite(f'Body_cache\\{file_input}_{formatted_number}.png', save_img)
-        except Exception as error:
-            logging.critical(error)
+            copyfile(f"Categorize_pictures\\{file_input}", f"Body_cache\\{file_input}")
 
-    logging.info('The split is complete')
 
+    # 对图片进行人脸截取，准备开始分类
     logging.info('Begin to recognize the face of the picture...')
     files_list = os.listdir('Body_cache')
     for i in tqdm(range(len(files_list))):
         try:
             file_input = files_list[i]
             img = cv2.imread(f'Body_cache\\{file_input}')
-            result = detect_faces(f'Body_cache\\{file_input}', iou_threshold=0.4, max_infer_size=896)
+            result = detect_faces(f'Body_cache\\{file_input}', iou_threshold=0.4)
             for h in range(len(result)):
                 coordinate_list = (result[h])[0]
                 save_img = img[int(coordinate_list[1]):int(coordinate_list[3]),
