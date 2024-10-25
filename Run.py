a5f51e4048fa8830e6b9c6b0897883d11c117a4b
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2024-10-25 14:50:09 +0800

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from Thread_scheduling import Thread_calls
 from log import get_logger
 import logging as log_use
@@ -8,13 +9,16 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-process_number', type=int, help='开启线程数', default=1)
-parser.add_argument('-confidence', type=float, help='置信度', default=0.5)
-parser.add_argument('-body_cut', type=bool, help='开启多人裁剪', default=False)
+parser.add_argument('-process_number', type=int, help='Number of threads enabled', default=1)
+parser.add_argument('-confidence', type=float, help='Confidence', default=0.5)
+parser.add_argument('-body_cut', type=bool, help='Turn on multi-person cropping', default=False)
+
 args = parser.parse_args()
 
 logging = get_logger(log_use.INFO)
 logging.info('Delete the folder')
+
+# 判断文件夹是否存在并删除缓存文件夹
 if os.path.isdir('Categories'):
     shutil.rmtree('Categories')
 if os.path.isdir('Cache'):
@@ -23,6 +27,8 @@ if os.path.isdir('Face_cache'):
     shutil.rmtree('Face_cache')
 if os.path.isdir('Body_cache'):
     shutil.rmtree('Body_cache')
+
+# 重新建立文件夹
 logging.info('Build folder')
 os.mkdir('Categories')
 os.mkdir('Cache')
@@ -30,7 +36,13 @@ os.mkdir('Face_cache')
 os.mkdir('Body_cache')
 
 try:
-    Thread_calls(process_src='Body_cache', process_number=args.process_number, confidence=args.confidence,
-                 body_cut=args.body_cut)
+    if args.body_cut:
+        Thread_calls(process_src='Body_cache', process_number=args.process_number, confidence=args.confidence,
+                     body_cut=args.body_cut)
+
+    if not args.body_cut:
+        Thread_calls(process_src='Categorize_pictures', process_number=args.process_number, confidence=args.confidence,
+                     body_cut=args.body_cut)
+
 except Exception as error:
     logging.critical(error)
